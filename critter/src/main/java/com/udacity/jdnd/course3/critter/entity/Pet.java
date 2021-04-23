35266665074ca958fc74a9f99d569ad4141d21a2
farahmadii <33867973+farahmadii@users.noreply.github.com>
2021-04-23 11:57:42 +0200

    solved the problem of inconsistency between PetDTo and Pet by matching properties names: petId -> id in Pet entity

--- critter/src/main/java/com/udacity/jdnd/course3/critter/entity/Pet.java
+++ critter/src/main/java/com/udacity/jdnd/course3/critter/entity/Pet.java
@@ -1,19 +1,23 @@
 package com.udacity.jdnd.course3.critter.entity;
 
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.hibernate.annotations.Nationalized;
 
 import javax.persistence.*;
 import java.time.LocalDate;
-import java.util.Set;
 
 @Entity
 @Table(name="tblPet")
+@Setter @Getter @NoArgsConstructor @EqualsAndHashCode
 public class Pet {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long petId;
+    private Long id;
 
     @Enumerated(EnumType.STRING)
     @Column(name = "type", nullable = false)
@@ -23,15 +27,20 @@ public class Pet {
     private String name;
 
     //don't retrieve the owner if we don't need it
-    @OneToOne(fetch = FetchType.LAZY)//many pets can belong to one owner
-    @JoinColumn(name = "owner_id")  //map the join column in the plant table
-    private Owner owner;
+//    @ManyToOne(fetch = FetchType.LAZY)//many pets can belong to one owner
+//    @JoinColumn(name = "owner_id")  //map the join column in the plant table
+//    private Owner owner;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "schedule_id")
-    private Schedule schedule;
+      @ManyToOne(fetch = FetchType.LAZY)//targetEntity = Owner.class, cascade = CascadeType.ALL,
+      @JoinColumn(name = "owner_id")
+      private Owner owner;
+
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "schedule_id")
+//    private Schedule schedule;
 
     private LocalDate birthDate;
 
     private String notes;
+
 }
