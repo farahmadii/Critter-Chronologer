729f65e547983caa97594d41011e8371bbf28078
farahmadii <33867973+farahmadii@users.noreply.github.com>
2021-04-23 11:51:03 +0200

    excetion handling for not found resources (user, owner, employee, pet, schedule) and bad request with missing parameters

---
+++ critter/src/main/java/com/udacity/jdnd/course3/critter/exception/ResourceNotFoundException.java
@@ -0,0 +1,15 @@
+package com.udacity.jdnd.course3.critter.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Resource not found")
+public class ResourceNotFoundException extends RuntimeException{
+
+    public ResourceNotFoundException() {
+    }
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
