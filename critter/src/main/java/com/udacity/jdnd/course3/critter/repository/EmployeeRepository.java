9a71057fb81b52ac5fcffa5fe68785ed3f64ef5f
farahmadii <33867973+farahmadii@users.noreply.github.com>
2021-04-24 09:17:27 +0200

    added @Transactional at repository level

--- critter/src/main/java/com/udacity/jdnd/course3/critter/repository/EmployeeRepository.java
+++ critter/src/main/java/com/udacity/jdnd/course3/critter/repository/EmployeeRepository.java
@@ -2,15 +2,15 @@ package com.udacity.jdnd.course3.critter.repository;
 
 
 import com.udacity.jdnd.course3.critter.entity.Employee;
-import com.udacity.jdnd.course3.critter.entity.EmployeeSkill;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.DayOfWeek;
 import java.util.List;
-import java.util.Set;
 
 @Repository
+@Transactional
 public interface EmployeeRepository extends JpaRepository<Employee,Long> {
 
     public List<Employee> findEmployeesByDaysAvailable(DayOfWeek day);
--- critter/src/main/java/com/udacity/jdnd/course3/critter/repository/ScheduleRepository.java
+++ critter/src/main/java/com/udacity/jdnd/course3/critter/repository/ScheduleRepository.java
@@ -4,10 +4,12 @@ package com.udacity.jdnd.course3.critter.repository;
 import com.udacity.jdnd.course3.critter.entity.Schedule;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Repository
+@Transactional
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
 
     List<Schedule> getScheduleByPets_Id(Long id);
